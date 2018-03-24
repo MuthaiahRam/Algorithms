b92e0f67f12d0a55d85d23baf8f19b579178d66a
unknown <muthaiahram93@gmail.com>
2018-03-23 18:29:27 -0700

    array partition leet code

---
+++ Logics/src/com/leet/logics/ArrayPartition.java
@@ -0,0 +1,19 @@
+package com.leet.logics;
+
+import java.util.Arrays;
+
+public class ArrayPartition {
+
+	public static int arrayPairSum(int[] nums) {
+		Arrays.sort(nums);
+		int sum=0;
+		for(int i=0;i<nums.length;i=i+2){
+			sum+=nums[i];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(ArrayPartition.arrayPairSum(new int[] { 1, 4, 3, 2 }));
+	}
+}
